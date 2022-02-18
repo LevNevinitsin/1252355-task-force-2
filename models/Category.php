c0a2304f7e7f5cdcc62130cd89a077f9b12cb1a6
Lev <levnevinitsin@gmail.com>
2022-02-18 08:55:09 +0300

    Add models for entities

---
+++ models/Category.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $icon
+ *
+ * @property ChosenCategory[] $chosenCategories
+ * @property Task[] $tasks
+ * @property User[] $users
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'icon'], 'required'],
+            [['name', 'icon'], 'string', 'max' => 50],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'icon' => 'Icon',
+        ];
+    }
+
+    /**
+     * Gets query for [[ChosenCategories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChosenCategories()
+    {
+        return $this->hasMany(ChosenCategory::class, ['category_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::class, ['category_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Users]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::class, ['id' => 'user_id'])->viaTable('chosen_category', ['category_id' => 'id']);
+    }
+}
--- models/User.php
+++ models/User.php
@@ -2,7 +2,9 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     public $id;
     public $username;
@@ -70,6 +72,14 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     /**
      * {@inheritdoc}
      */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getId()
     {
         return $this->id;
@@ -100,5 +110,118 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     public function validatePassword($password)
     {
         return $this->password === $password;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'password', 'city_id', 'role_id'], 'required'],
+            [['city_id', 'role_id', 'fails_count'], 'integer'],
+            [['birthdate', 'date_registered'], 'safe'],
+            [['self_description'], 'string'],
+            [['name'], 'string', 'max' => 100],
+            [['email'], 'string', 'max' => 50],
+            [['password', 'photo'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 11],
+            [['telegram'], 'string', 'max' => 64],
+            [['email'], 'unique'],
+            [['city_id'], 'exist', 'skipOnError' => true, 'targetClass' => City::class, 'targetAttribute' => ['city_id' => 'id']],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'city_id' => 'City ID',
+            'birthdate' => 'Birthdate',
+            'photo' => 'Photo',
+            'phone' => 'Phone',
+            'telegram' => 'Telegram',
+            'self_description' => 'Self Description',
+            'role_id' => 'Role ID',
+            'fails_count' => 'Fails Count',
+            'date_registered' => 'Date Registered',
+        ];
+    }
+
+    /**
+     * Gets query for [[Categories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategories()
+    {
+        return $this->hasMany(Category::class, ['id' => 'category_id'])->viaTable('chosen_category', ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[ChosenCategories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChosenCategories()
+    {
+        return $this->hasMany(ChosenCategory::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[City]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCity()
+    {
+        return $this->hasOne(City::class, ['id' => 'city_id']);
+    }
+
+    /**
+     * Gets query for [[Responses]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getResponses()
+    {
+        return $this->hasMany(Response::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Role]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRole()
+    {
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::class, ['contractor_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Tasks0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks0()
+    {
+        return $this->hasMany(Task::class, ['customer_id' => 'id']);
     }
 }
