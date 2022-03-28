88a851f73d6733182c470383c447b4ec50019555
Lev <levnevinitsin@gmail.com>
2022-03-28 18:38:14 +0300

    Update task fixture

--- fixtures/templates/task.php
+++ fixtures/templates/task.php
@@ -12,7 +12,7 @@ return [
     'overview'       => $faker->sentence(),
     'description'    => $faker->paragraph(),
     'category_id'    => random_int(1, 8),
-    'city_id'        => random_int(1, 1087),
+    'city_id'        => random_int(1, 5) > 1 ? random_int(1, 1087) : NULL,
     'budget'         => random_int(50, 10000),
     'deadline'       => $faker->dateTimeBetween('+1 day', '+10 days')->format('Y-m-d'),
     'task_status_id' => $taskStatusId,
@@ -20,5 +20,5 @@ return [
     'contractor_id'  => $taskStatusId !== 1 && $taskStatusId !== 2 ? $contractorsIds[array_rand($contractorsIds)] : NULL,
     'score'          => $taskStatusId === 5 ? random_int(1, 5) : NULL,
     'feedback'       => $taskStatusId === 5 ? $faker->paragraph() : NULL,
-    'date_created'   => $faker->dateTimeBetween('-2 years')->format('Y-m-d H:i:s'),
+    'date_created'   => $faker->dateTimeBetween('-2 days')->format('Y-m-d H:i:s'),
 ];
