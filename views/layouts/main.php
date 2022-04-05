4ac5351ab24c9f6bc3385dba7ab582f59ebc97a1
Lev <levnevinitsin@gmail.com>
2022-04-05 15:26:46 +0300

    Update main layout
    Added actual username in header.
    Added url address to logo.
    Added logout.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,6 +9,8 @@ use yii\bootstrap4\Html;
 
 AppAsset::register($this);
 MainAsset::register($this);
+
+$currentPage = Yii::$app->request->getPathInfo();
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -26,10 +28,10 @@ MainAsset::register($this);
 
 <header class="page-header">
     <nav class="main-nav">
-        <a href='#' class="header-logo">
+        <a <?= $currentPage === 'tasks' ? '' : 'href="/tasks"' ?> class="header-logo">
             <img class="logo-image" src="/img/logotype.png" width=227 height=60 alt="taskforce">
         </a>
-        <?php $isRegistrationPage = Yii::$app->request->getUrl() === '/registration' ?>
+        <?php $isRegistrationPage = $currentPage === 'registration' ?>
         <?php if (!$isRegistrationPage): ?>
         <div class="nav-wrapper">
             <ul class="nav-list">
@@ -55,7 +57,7 @@ MainAsset::register($this);
             <img class="user-photo" src="/img/man-glasses.png" width="55" height="55" alt="Аватар">
         </a>
         <div class="user-menu">
-            <p class="user-name">Василий</p>
+            <p class="user-name"><?= \Yii::$app->user->identity->name ?></p>
             <div class="popup-head">
                 <ul class="popup-menu">
                     <li class="menu-item">
@@ -65,7 +67,7 @@ MainAsset::register($this);
                         <a href="#" class="link">Связаться с нами</a>
                     </li>
                     <li class="menu-item">
-                        <a href="#" class="link">Выход из системы</a>
+                        <a href="/users/logout" class="link">Выход из системы</a>
                     </li>
                 </ul>
             </div>
