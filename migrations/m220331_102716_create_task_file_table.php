c2d58eb422ae50431df887cda62a27a5eef4f9cc
Lev <levnevinitsin@gmail.com>
2022-04-09 11:57:10 +0300

    Update task and task_file table initial migrations

--- migrations/m220331_102716_create_task_file_table.php
+++ migrations/m220331_102716_create_task_file_table.php
@@ -19,6 +19,7 @@ class m220331_102716_create_task_file_table extends Migration
             'id' => $this->primaryKey(),
             'task_id' => $this->integer()->notNull(),
             'path' => $this->string(255)->notNull(),
+            'original_name' => $this->string(255)->notNull(),
             'date_created' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
         ]);
 
